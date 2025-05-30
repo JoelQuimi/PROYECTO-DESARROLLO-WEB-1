4040c9b8f6dc47ebd9b67475e17cd884575e8f58
andresaroni <marcoaroni10@gmail.com>
2025-05-29 20:25:08 -0500

    Proyecto Estructurado correctamente

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const hamburguesa = document.querySelector(".hamburguesa");
 const navMenu = document.querySelector(".nav-menu");
+const links = document.querySelector('.nav-menu');
+const a = document.querySelectorAll('.nav-link');
 
 hamburguesa.addEventListener("click", () => {
   hamburguesa.classList.toggle("active");
   navMenu.classList.toggle("active");
+});
+
+a.forEach(link => {
+  link.addEventListener('click', () => {
+    hamburguesa.classList.remove('active');
+    navMenu.classList.remove('active');
+  });
 });
